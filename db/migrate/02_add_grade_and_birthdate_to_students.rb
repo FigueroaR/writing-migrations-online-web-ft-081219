01fadd71280752163f529acc749281b4a82d0e72
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-10-13 01:57:00 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,7 +1,7 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   def change 
     add_column :student, :grade, :nteger 
-    add_column 
+    add_column :student, :birthdate, :string 
   end 
   
   
